c92e2cd2107169ceee3f50b8f7764bfb0e27c442
ankit <ankitajnavi@gmail.com>
2025-01-01 16:13:59 +0530

    Project Management System frontend Completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import * as tailwindAnimate from "tailwindcss-animate"
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
@@ -56,5 +57,5 @@ export default {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindAnimate],
 }
